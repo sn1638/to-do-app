060916d33385653531dc85d09fb3b8ee02e4d28d
sn1638 <sachin111mrj@gmail.com>
2025-08-06 22:39:17 +0530

    made responsive

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -4,7 +4,7 @@ import question from "./array"
 function Quiz() {
     const [num,setnum] = useState(0)
     const [message,setmessage]=useState("")
-
+    
     const [bg1,setbg1]=useState(`bg-purple-500`)
     const [bg2,setbg2]=useState(`bg-purple-500`)
     const [bg3,setbg3]=useState(`bg-purple-500`)
@@ -89,24 +89,24 @@ const btn4click = ()=>{
     }}
   return (
     <>
-        <div className='p-5 w-full h-full'>
-           <div className='shadow-black shadow-xl rounded-3xl p-5 bg-cyan-700 text-white text-4xl flex items-center justify-center'>
+        <div className='w-full h-full p-5'>
+           <div className=' lg:h-1/5 md:h-1/3 sm:h-1/2 shadow-black shadow-xl rounded-3xl p-6 bg-cyan-700 text-white sm:text-3xl md:text-4xl lg:text-4xl flex items-center justify-center m-5'>
                {question[num].question}
             </div > 
-            <div className=' p-7 text-black text-xl flex h-1/6 justify-around items-center'>
-            <button onClick={btn1click} className={`shadow-black shadow-2xl cursor-pointer max-w-1/5 min-w-1/5 rounded-2xl p-5 ${bg1}`}>{`(a)${question[num].options[0]}`}</button> 
-            <button onClick={btn2click} className={`shadow-black shadow-2xl ${bg1.replace("bg-","")} cursor-pointer max-w-1/5 min-w-1/5 rounded-2xl p-5 ${bg2}`}>{`(b)${question[num].options[1]}`}</button> 
-            <button onClick={btn3click} className={`shadow-black shadow-2xl ${bg1.replace("bg-","")} cursor-pointer max-w-1/5 min-w-1/5 rounded-2xl p-5 ${bg3}`}>{`(c)${question[num].options[2]}`}</button> 
-            <button onClick={btn4click} className={`shadow-black shadow-2xl ${bg1.replace("bg-","")} cursor-pointer max-w-1/5 min-w-1/5 rounded-2xl p-5 ${bg4}`}>{`(d)${question[num].options[3]}`}</button> 
+            <div className='lg:p-7 sm:p-1 w-full text-black text-xl sm:flex-col md:flex-col lg:flex-row flex-col flex md:h-1/5 sm:h-1/4 lg:h-1/6 justify-around items-center'>
+            <button onClick={btn1click} className={`shadow-black shadow-2xl cursor-pointer sm:w-full md:w-1/2 lg:w-1/5 rounded-2xl p-5 m-4 ${bg1}`}>{`(a)${question[num].options[0]}`}</button> 
+            <button onClick={btn2click} className={`shadow-black shadow-2xl ${bg1.replace("bg-","")} cursor-pointer sm:w-full md:w-1/2 lg:w-1/5  m-4 min-w-1/5 rounded-2xl p-5 ${bg2}`}>{`(b)${question[num].options[1]}`}</button> 
+            <button onClick={btn3click} className={`shadow-black shadow-2xl ${bg1.replace("bg-","")} cursor-pointer   m-4 sm:w-full md:w-1/2 lg:w-1/5 rounded-2xl p-5 ${bg3}`}>{`(c)${question[num].options[2]}`}</button> 
+            <button onClick={btn4click} className={`shadow-black shadow-2xl ${bg1.replace("bg-","")} cursor-pointer m-4 sm:w-full md:w-1/2 lg:w-1/5 rounded-2xl p-5 ${bg4}`}>{`(d)${question[num].options[3]}`}</button> 
             </div>
-            <div className='text-red-800 p-7 text-6xl flex items-center justify-center'>
+            <div className='text-red-800 p-7 sm:text:4xl md:text-5xl lg:text-6xl flex items-center justify-center'>
                 {message}
             </div>
-            <div className='fixed bottom-0 right-0 w-1/3 p-7 flex justify-between'>
-                <button onClick={subclick} className='cursor-pointer hover: shadow-black shadow-xl rounded-2xl p-2 bg-amber-400'>submit</button>
+            <footer className='fixed bottom-0 right-0 sm:w-full md:w-1/2 lg:w-1/3 p-7 flex justify-between'>
+                <button onClick={subclick} className='sm:w-1/4 md:w-/4 lg:w-1/6 cursor-pointer hover: shadow-black shadow-xl rounded-2xl p-2 bg-amber-400'>submit</button>
                 <button onClick={prevclick} className='cursor-pointer hover: shadow-black shadow-xl rounded-2xl p-2 bg-blue-600'>previous questin</button>
                 <button onClick={nextclick} className='cursor-pointer shadow-black shadow-xl rounded-2xl p-2 bg-green-500'>next question</button>
-            </div>
+            </footer>
         </div>
     </>
   )
